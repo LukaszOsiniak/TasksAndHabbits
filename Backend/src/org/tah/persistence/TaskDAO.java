6c9a45db72e106da16a55137d7c9e1824a0795f9
LukaszOsiniak <lukasz.osiniak@gmail.com>
2021-05-02 19:45:40 +0200

    Add Logger, Parametrized queries adn log4j2.xml

--- Backend/src/org/tah/persistence/TaskDAO.java
+++ Backend/src/org/tah/persistence/TaskDAO.java
@@ -1,68 +1,64 @@
 package org.tah.persistence;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.tah.model.Task;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TaskDAO {
 
-    private static final String INSERT_TASK_SQL_PART_ONE = "INSERT INTO tasks VALUES (";
-    private static final String UPDATE_TASK_SQL = "UPDATE tasks SET ";
-    private static final String DELETE_TASK_SQL = "DELETE FROM tasks WHERE name = ";
+    private static final Logger LOG = LogManager.getLogger(TaskDAO.class);
+    private static final String INSERT_SQL = "INSERT INTO tasks VALUES (?, ?, ?)";
+    private static final String UPDATE_SQL = "UPDATE tasks SET name = ? , status = ? WHERE id = ?";
+    private static final String DELETE_SQL = "DELETE FROM tasks WHERE name = ?";
 
     public void insert(Task task) {
-        String insertSQL = INSERT_TASK_SQL_PART_ONE;
-        insertSQL += "'" + task.getName() + "' , '" +
-                task.getStatus() + "' , " +
-                task.getTaskId() + ")";
-        System.out.println(insertSQL);
 
         DbConnection dbConnection = new DbConnection();
 
         try (Connection connection = dbConnection.getConnection();
-             Statement statement = connection.createStatement()) {
+             PreparedStatement statement = connection.prepareStatement(INSERT_SQL)) {
+            statement.setString(1, task.getName());
+            statement.setString(2, task.getStatus().name());
+            statement.setInt(3, task.getTaskId());
 
-            int rowCount = statement.executeUpdate(insertSQL);
-            System.out.println("Row count for the insert: " + rowCount);
+            int rowCount = statement.executeUpdate();
+            LOG.info("Number of inserted rows" + rowCount);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            LOG.error(e, e);
         }
     }
 
     public void update(Task task) {
-        String updateSQL = UPDATE_TASK_SQL;
-        updateSQL += "name = '" + task.getName() + "', " + "status = '" + task.getStatus() + "'" + " WHERE id = " + task.getTaskId();
-        System.out.println(updateSQL);
 
         DbConnection dbConnection = new DbConnection();
 
         try (Connection connection = dbConnection.getConnection();
-             Statement statement = connection.createStatement()) {
-            int rowCount = statement.executeUpdate(updateSQL);
-            System.out.println("Rows updated: " + rowCount);
+             PreparedStatement statement = connection.prepareStatement(UPDATE_SQL)) {
+            statement.setString(1, task.getName());
+            statement.setString(2, task.getStatus().name());
+            statement.setInt(3, task.getTaskId());
+            int rowCount = statement.executeUpdate();
+            LOG.info("Number of updated rows" + rowCount);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            LOG.error(e, e);
         }
     }
 
     public void delete(Task task) {
-        String deleteSQL = DELETE_TASK_SQL;
-        deleteSQL += "'" + task.getName() + "'";
-        System.out.println(deleteSQL);
 
         DbConnection dbConnection = new DbConnection();
 
         try (Connection connection = dbConnection.getConnection();
-             Statement statement = connection.createStatement()) {
-            int rowCount = statement.executeUpdate(deleteSQL);
+             PreparedStatement statement = connection.prepareStatement(DELETE_SQL)) {
+            statement.setString(1, task.getName());
+            int rowCount = statement.executeUpdate();
+            LOG.info("Number of deleted rows" + rowCount);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            LOG.error(e, e);
         }
     }
 }
-
